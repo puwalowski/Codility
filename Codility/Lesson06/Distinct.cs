39ed323829e46d109b61327ec1b63db10e2f0aca
Matt <mateusz.puwalowski@bellin.com>
2019-04-16 12:15:54 +0200

    Distinct

---
+++ Codility/Lesson06/Distinct.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Codility.Lesson06
+{
+    public static class Distinct
+    {
+        [Test]
+        public static void Test()
+        {
+            var result = Run(new[] { 2, 1, 1, 2, 3, 1 });
+            Assert.AreEqual(3, result);
+        }
+
+        private static int Run(int[] A)
+        {
+            var hashSet = new HashSet<int>();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (!hashSet.Contains(A[i]))
+                {
+                    hashSet.Add(A[i]);
+                }
+            }
+
+            return hashSet.Count;
+        }
+    }
+}
